fa3f27522843764367d2667b518385b3ae08bcc5
31207 <1692038362@qq.com>
2026-01-17 17:53:53 +0800

    fix: 修改了'jumpscare'的一些参数

--- src/memes/jumpscare.rs
+++ src/memes/jumpscare.rs
@@ -1,14 +1,19 @@
-use skia_safe::{Image, Matrix, Point, font_style::Width, surface};
+use skia_safe::{Image, Matrix, Point};
 
 use meme_generator_core::error::Error;
 use meme_generator_utils::{
     builder::InputImage,
     encoder::{FrameAlign, GifInfo, make_gif_or_combined_gif},
     image::{Fit, ImageExt},
-    tools::{load_image, local_date},
+    tools::{local_date},
 };
 
 use crate::{options::NoOptions, register_meme};
+
+const GIF_INFO: GifInfo = GifInfo {
+    duration: 0.026,
+    frame_num: 18,
+};
 
 fn double_heartbeat(x: f32) -> f32 {
     // 基础配置
@@ -28,8 +33,8 @@ fn double_heartbeat(x: f32) -> f32 {
 fn jumpscare(images: Vec<InputImage>, _: Vec<String>, _: NoOptions) -> Result<Vec<u8>, Error> {
     let func = |i: usize, images: Vec<Image>| {
         let mut pic = images[0].clone();
-        if images[0].width() > 400 || images[0].height() > 400 {
-            pic = images[0].resize_fit((400, 380), Fit::Contain);
+        if images[0].width() > 280 || images[0].height() > 280 {
+            pic = images[0].resize_fit((280, 280), Fit::Contain);
         }
         let mut surface = pic.to_surface();
         let canvas = surface.canvas();
@@ -42,7 +47,7 @@ fn jumpscare(images: Vec<InputImage>, _: Vec<String>, _: NoOptions) -> Result<Ve
             Point::new(0.0, h),
         ];
 
-        let offset = h * 0.2; // 缩进量
+        let offset = h * 0.25; // 缩进量
         let dst = [
             Point::new(offset, offset * 1.5),     // 左上向右移
             Point::new(w - offset, offset * 1.5), // 右上向左移
@@ -51,7 +56,7 @@ fn jumpscare(images: Vec<InputImage>, _: Vec<String>, _: NoOptions) -> Result<Ve
         ];
         let matrix = Matrix::from_poly_to_poly(&src, &dst).unwrap_or_default();
         canvas.concat(&matrix);
-        let factor = 1.0 + double_heartbeat(i as f32 / 12.0) * 2.0;
+        let factor = 1.0 + double_heartbeat(i as f32 / GIF_INFO.frame_num as f32) * 2.0;
         let width = pic.width() as f32 * factor;
         let width = width as i32;
         canvas.draw_image(
@@ -65,11 +70,8 @@ fn jumpscare(images: Vec<InputImage>, _: Vec<String>, _: NoOptions) -> Result<Ve
     make_gif_or_combined_gif(
         images,
         func,
-        GifInfo {
-            frame_num: 12,
-            duration: 0.04,
-        },
-        FrameAlign::ExtendLoop,
+        GIF_INFO,
+        FrameAlign::NoExtend,
     )
 }
 
